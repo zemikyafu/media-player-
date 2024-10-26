b60b936ab895f3313e46fecb6b3df698b43990d5
zemikyafu <zemikfcb@gmail.com>
2024-10-26 15:14:30 +0300

    Create presentaion layer userController

---
+++ src/main/java/org/media_player/presentation/controllers/UserController.java
@@ -0,0 +1,43 @@
+package org.media_player.presentation.controllers;
+
+import org.media_player.application.services.UserService;
+import org.media_player.domain.entities.user.Role;
+import org.media_player.domain.entities.user.User;
+
+import java.util.List;
+
+public class UserController {
+    private UserService userService;
+
+    UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public List<User> getAllUser() {
+        return userService.findAllUser();
+    }
+
+    public User getUser(String email) {
+        return userService.findByEmail(email);
+    }
+
+    public void addUser(String name, String email, String password) {
+        userService.signup(name, email, password);
+
+    }
+
+    public void login(String email, String password) {
+        userService.login(email, password);
+
+    }
+
+    public void updateUser(User user, Role role) {
+        userService.updateRole(user, role);
+    }
+
+    public void deleteUser(User user) {
+        userService.deleteUser(user);
+    }
+
+
+}
